b74b848836a05f83339dbc5d1b57c30ed5164cde
Ishan-sopho <bhanuka.ishan@gmail.com>
2017-06-24 21:54:42 +0530

    prettier printing in spoj

--- spoj/spoj.py
+++ spoj/spoj.py
@@ -16,9 +16,28 @@ def problem(url):
         print soup.select('h2#problem-name')[0].text
         print
         body = soup.select('div#problem-body')[0].text
-        index = min(body.find('Example'), body.find('Input'), body.find('Output'))
-        print textwrap.fill(body[0:index], width=80)
-        print
-        print body[index::1]
+        if body.find('Input') < body.find('Example'):
+            in_index, out_index, info_index, eg_index = body.find('Input'), body.find('Output'),\
+                                                        body.find('Information'), body.find('Example')
+            print textwrap.fill(body[0:in_index], width=80)
+            print
+            print 'Input'
+            print textwrap.fill(body[in_index+5:out_index], width=80)
+            print
+            print 'Output'
+            print textwrap.fill(body[out_index+6: eg_index], width=80)
+            print
+            if info_index != -1:
+                print body[eg_index:info_index]
+            else:
+                print body[eg_index::1]
+        else:
+            eg_index, info_index = body.find('Example'), body.find('Information')
+            print textwrap.fill(body[0:eg_index], width=80)
+            print
+            if info_index != -1:
+                print body[eg_index:info_index]
+            else:
+                print body[eg_index::1]
 
 problem(sys.argv[1])
