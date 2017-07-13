238adab041d69de76e7887710a541d948f076dc8
Divesh Uttamchandani <diveshuttamchandani@gmail.com>
2017-07-13 17:37:52 +0530

    BROKEN contd.. iarcs check status sort of working

--- termicoder/judges/iarcs/modules/submit.py
+++ termicoder/judges/iarcs/modules/submit.py
@@ -3,6 +3,8 @@ import os
 import json
 import click
 import requests
+from bs4 import BeautifulSoup
+import time
 import termicoder.judges.iarcs.modules.setup as setup_module
 import termicoder.judges.iarcs.modules.utils.session as session
 import termicoder.utils.display as display
@@ -15,13 +17,28 @@ lang_map={
 ".cc":"cpp",
 ".c++":"cpp"
 }
+
 def get_submission_code(html):
-    print("get_submission_code not implemented yet")
+    soup=BeautifulSoup(html,"html.parser")
+    code_body=str(soup.find(id="maincontent").get_text(strip=True))
+    code=(code_body.split(' ')[2])
+    return(code)
 
 def check_status(code):
-    print("check_status not implemented yet")
+    delay=1
+    iarcs_session=session.iarcs_session
+    url="http://opc.iarcs.org.in/index.php/results/"+str(code)
 
-    
+    status="compiling"
+    while("running" in status.lower() or "compiling" in status.lower()):
+        r=iarcs_session.get(url)
+        soup=BeautifulSoup(r.text,"html.parser")
+        body=soup.find(id="maincontent")
+        status=body.findAll("p")[1].text.split(' ',1) [1]
+        time.sleep(delay)
+    points=body.findAll("p")[2].text.rsplit(' ',1)[1]
+    return status, points
+
 def submit(code_file):
     iarcs_session=session.iarcs_session
     problem_file=open(".problem")
@@ -61,7 +78,11 @@ def submit(code_file):
     # TODO: test before submit
     r=iarcs_session.post(submit_url,files=files,data=data)
     display.normal("\tDone")
-    display.normal("retriving status (you can continue your work in another tab)...")
+    click.echo("retriving status (you can continue your work in another tab)...",
+                nl=False)
 
     submission_code=get_submission_code(r.text)
-    a=check_status(submission_code)
+    status,points=check_status(submission_code)
+    click.echo("\tDone")
+    click.echo("status: "+status)
+    click.echo("points: "+points)
