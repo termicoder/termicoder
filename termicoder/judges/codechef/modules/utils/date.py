fa9459498f731c13f8eab256126e0dacbc9585bd
Divesh Uttamchandani <diveshuttamchandani@gmail.com>
2017-07-22 01:12:42 +0530

    date utility for codechef added helps in formatting epoch time

---
+++ termicoder/judges/codechef/modules/utils/date.py
@@ -0,0 +1,10 @@
+import datetime
+
+def get_date_time(epoch):
+    return (get_date(epoch),get_time(epoch))
+
+def get_date(epoch):
+    return datetime.datetime.fromtimestamp(int(epoch)).strftime('%d %h %y')
+
+def get_time(epoch):
+    return datetime.datetime.fromtimestamp(int(epoch)).strftime('%I:%M %p')
