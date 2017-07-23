d8995e272479a73c13cf2650b079311dd9a7fcce
Divesh Uttamchandani <diveshuttamchandani@gmail.com>
2017-07-23 13:17:38 +0530

    files configuring setup

--- setup.py
+++ setup.py
@@ -1,8 +1,39 @@
-from setuptools import setup, find_packages
+from setuptools import setup,find_packages
+
+def readme():
+    try:
+        with open('README.rst') as f:
+            return f.read()
+    except:
+        pass
 
 setup(
     name='termicoder',
-    version='1.0',
+    version='0.1.0',
+    url='https://github.com/termicoder/termicoder',
+    author='Divesh Uttamchandani',
+    author_email='diveshuttamchandani@gmail.com',
+    license='MIT',
+    description='a CLI to view, code and submit problems directly from terminal',
+    long_description=readme(),
+    keywords='competetive iarcs codechef oj',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        
+        'Intended Audience :: Programmers',
+        'Topic :: Competetive Programming :: Automation',
+        
+        'License :: OSI Approved :: MIT License',
+        
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+      ],
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
